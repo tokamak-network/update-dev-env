776e4b8f4777f38cc967a6bfad4e1ad362a97a32
blackcow1987 <blackcow@graylab.team>
2025-05-27 01:59:41 +0900

    added test cases for AuthControlSeigManager.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,28 +5,15 @@ import 'hardhat-deploy'
 import type { HardhatUserConfig } from 'hardhat/config'
 import 'solidity-coverage'
 import 'tsconfig-paths/register'
+import '@typechain/hardhat'
 
 dotenv.config()
 
-// MultisigWallet  0xE3F72E959834d0A72aFb2ea79F5ec2b4243d2d95
-
-// DAOCommitteeProxy 0xDD9f0cCc044B0781289Ee318e5971b0139602C26
-// DAOAgendaManager 0xcD4421d082752f363E1687544a09d5112cD4f484
-// DepositManagerProxy 0x0b58ca72b12f01fc05f8f252e226f3e2089bd00e
-// SeigManagerProxy 0x0b55a0f463b6defb81c6063973763951712d0e5f
-// Layer2RegistryProxy 0x7846c2248a7b4de77e9c2bae7fbb93bfc286837b
-// CoinageFactory 0xe8fae91b80dd515c3d8b9fc02cb5b2ecfddabf43
-// CandidateFactoryProxy 0x9fc7100a16407ee24a79c834a56e6eca555a5d7c
-
-// L1BridgeRegistryProxy  0x39d43281A4A5e922AB0DCf89825D73273D8C5BA4
-// CandidateAddOnFactoryProxy 0xFA8ce5caF456115E72B96E5074769b8f66AA5861
-// Layer2ManagerProxy  0xD6Bf6B2b7553c8064Ba763AD6989829060FdFC1D
-// OperatorManagerFactory 0xAf86b21edDdC78ea27E23A7F2151d60d4e069450
-
-// TON: 0x2be5e8c109e2197D077D13A82dAead6a9b3433C5
-// WTON: 0xc4A11aaf6ea915Ed7Ac194161d2fC9384F15bff2
-
 const config: HardhatUserConfig = {
+  typechain: {
+    outDir: 'typechain-types',
+    target: 'ethers-v6'
+  },
   namedAccounts: {
     L1_BRIDGE_REGISTRY: {
       hardhat: '0x39d43281A4A5e922AB0DCf89825D73273D8C5BA4'
---
+++ test/common/AuthControlL1BridgeRegistry.ts
@@ -0,0 +1,275 @@
+import type { AuthControlL1BridgeRegistry } from '@contracts/common/AuthControlL1BridgeRegistry'
+import { AuthControlL1BridgeRegistry__factory } from '@factories/common/AuthControlL1BridgeRegistry__factory'
+import type { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers'
+import { loadFixture, setStorageAt } from '@nomicfoundation/hardhat-network-helpers'
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+
+describe('AuthControlL1BridgeRegistry', () => {
+  let authControlL1BridgeRegistry: AuthControlL1BridgeRegistry
+  let admin: HardhatEthersSigner
+  let newAdmin: HardhatEthersSigner
+  let nonAdmin: HardhatEthersSigner
+  let manager: HardhatEthersSigner
+  let newManager: HardhatEthersSigner
+  let nonManager: HardhatEthersSigner
+  let registrant: HardhatEthersSigner
+  let newRegistrant: HardhatEthersSigner
+  let nonRegistrant: HardhatEthersSigner
+
+  const snapshot = async () => {
+    const factory = new AuthControlL1BridgeRegistry__factory().connect(admin)
+    const authControlL1BridgeRegistry = await factory.deploy()
+
+    const role = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    const encodedOuter = ethers.AbiCoder.defaultAbiCoder().encode(['bytes32', 'uint256'], [role, 1])
+    const outerSlot = ethers.keccak256(encodedOuter)
+    const encodedMember = ethers.AbiCoder.defaultAbiCoder().encode(
+      ['address', 'uint256'],
+      [admin.address, BigInt(outerSlot)]
+    )
+    const memberSlot = ethers.keccak256(encodedMember)
+    await setStorageAt(authControlL1BridgeRegistry.target.toString(), memberSlot, ethers.zeroPadValue('0x01', 32))
+
+    await authControlL1BridgeRegistry.addManager(manager)
+    await authControlL1BridgeRegistry.connect(manager).addRegistrant(registrant)
+
+    return authControlL1BridgeRegistry
+  }
+
+  beforeEach(async () => {
+    ;[admin, manager, registrant, newRegistrant, nonAdmin, nonManager, newManager, nonRegistrant, newAdmin] =
+      await ethers.getSigners()
+    authControlL1BridgeRegistry = await loadFixture(snapshot)
+  })
+
+  describe('Test for addAdmin', () => {
+    it('should fail when non-admin tries to add admin', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).addAdmin(nonAdmin)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when already granted', async () => {
+      await expect(authControlL1BridgeRegistry.addAdmin(admin)).to.be.revertedWith('already granted')
+    })
+
+    it('should add admin', async () => {
+      await authControlL1BridgeRegistry.addAdmin(newAdmin)
+      expect(await authControlL1BridgeRegistry.isAdmin(newAdmin)).to.equal(true)
+    })
+  })
+
+  describe('Test for addManager', () => {
+    it('should fail when non-admin tries to add manager', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonManager).addManager(nonManager)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when already granted', async () => {
+      await expect(authControlL1BridgeRegistry.addManager(manager)).to.be.revertedWith('already granted')
+    })
+
+    it('should add manager', async () => {
+      await authControlL1BridgeRegistry.addManager(newManager)
+      expect(await authControlL1BridgeRegistry.isManager(newManager)).to.equal(true)
+    })
+  })
+
+  describe('Test for addRegistrant', () => {
+    it('should fail when non-manager tries to add registrant', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonManager).addRegistrant(nonRegistrant)).to.be.revertedWith(
+        'AuthControl: Caller is not a manager'
+      )
+    })
+
+    it('should fail when already granted', async () => {
+      await expect(authControlL1BridgeRegistry.connect(manager).addRegistrant(registrant)).to.be.revertedWith(
+        'already granted'
+      )
+    })
+
+    it('should add registrant', async () => {
+      await authControlL1BridgeRegistry.connect(manager).addRegistrant(newRegistrant)
+      expect(await authControlL1BridgeRegistry.connect(manager).isRegistrant(newRegistrant)).to.equal(true)
+    })
+  })
+
+  describe('Test for removeAdmin', () => {
+    it('should fail when non-admin tries to remove admin', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).removeAdmin(admin)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when already not granted', async () => {
+      await expect(authControlL1BridgeRegistry.removeAdmin(nonAdmin)).to.be.revertedWith('already not granted')
+    })
+
+    it('should remove admin', async () => {
+      await authControlL1BridgeRegistry.removeAdmin(admin)
+      expect(await authControlL1BridgeRegistry.isAdmin(admin)).to.equal(false)
+    })
+  })
+
+  describe('Test for removeManager', () => {
+    it('should fail when non-admin tries to remove manager', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).removeManager(manager)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when already not granted', async () => {
+      await expect(authControlL1BridgeRegistry.removeManager(nonManager)).to.be.revertedWith('already not granted')
+    })
+
+    it('should remove manager', async () => {
+      await authControlL1BridgeRegistry.removeManager(manager)
+      expect(await authControlL1BridgeRegistry.isManager(manager)).to.equal(false)
+    })
+  })
+
+  describe('Test for removeRegistrant', () => {
+    it('should fail when non-manager tries to remove registrant', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonManager).removeRegistrant(registrant)).to.be.revertedWith(
+        'AuthControl: Caller is not a manager'
+      )
+    })
+
+    it('should fail when already not granted', async () => {
+      await expect(authControlL1BridgeRegistry.connect(manager).removeRegistrant(nonRegistrant)).to.be.revertedWith(
+        'already not granted'
+      )
+    })
+
+    it('should remove registrant', async () => {
+      await authControlL1BridgeRegistry.connect(manager).removeRegistrant(registrant)
+      expect(await authControlL1BridgeRegistry.connect(manager).isRegistrant(registrant)).to.equal(false)
+    })
+  })
+
+  describe('Test for transferAdmin', () => {
+    it('should fail when non-admin tries to transfer admin role', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).transferAdmin(newAdmin.address)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when transfer admin role to same address', async () => {
+      await expect(authControlL1BridgeRegistry.transferAdmin(admin.address)).to.be.revertedWith(
+        'Accessible: same admin'
+      )
+    })
+
+    it('should fail when transfer admin role to zero address', async () => {
+      await expect(authControlL1BridgeRegistry.transferAdmin(ethers.ZeroAddress)).to.be.revertedWith(
+        'Accessible: zero address'
+      )
+    })
+
+    it('should fail when transfer admin role to admin', async () => {
+      await authControlL1BridgeRegistry.addAdmin(newAdmin.address)
+      await expect(authControlL1BridgeRegistry.transferAdmin(newAdmin.address)).to.be.revertedWith('already granted')
+    })
+
+    it('should transfer admin role', async () => {
+      await authControlL1BridgeRegistry.transferAdmin(newAdmin.address)
+      expect(await authControlL1BridgeRegistry.isAdmin(newAdmin)).to.equal(true)
+      expect(await authControlL1BridgeRegistry.isAdmin(admin)).to.equal(false)
+    })
+  })
+
+  describe('Test for renounceOwnership', () => {
+    it('should fail when non-admin tries to renounce ownership', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).renounceOwnership()).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should renounce ownership', async () => {
+      await authControlL1BridgeRegistry.renounceOwnership()
+      expect(await authControlL1BridgeRegistry.isAdmin(admin)).to.equal(false)
+    })
+  })
+
+  describe('Test for renounceManager', () => {
+    it('should fail when non-manager tries to renounce manager', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonManager).renounceManager()).to.be.revertedWith(
+        'already not granted'
+      )
+    })
+
+    it('should renounce manager', async () => {
+      await authControlL1BridgeRegistry.connect(manager).renounceManager()
+      expect(await authControlL1BridgeRegistry.isManager(manager)).to.equal(false)
+    })
+  })
+
+  describe('Test for renounceRegistrant', () => {
+    it('should fail when non-registrant tries to renounce registrant', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonRegistrant).renounceRegistrant()).to.be.revertedWith(
+        'already not granted'
+      )
+    })
+
+    it('should renounce registrant', async () => {
+      await authControlL1BridgeRegistry.connect(registrant).renounceRegistrant()
+      expect(await authControlL1BridgeRegistry.isRegistrant(registrant)).to.equal(false)
+    })
+  })
+
+  describe('Test for revokeManager', () => {
+    it('should fail when non-admin tries to revoke manager', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).revokeManager(manager)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when not granted', async () => {
+      await expect(authControlL1BridgeRegistry.connect(admin).revokeManager(nonManager)).to.be.revertedWith(
+        'already not granted'
+      )
+    })
+
+    it('should revoke manager', async () => {
+      await authControlL1BridgeRegistry.connect(admin).revokeManager(manager)
+      expect(await authControlL1BridgeRegistry.isManager(manager)).to.equal(false)
+    })
+  })
+
+  describe('Test for revokeRegistrant', () => {
+    it('should fail when non-admin tries to revoke registrant', async () => {
+      await expect(authControlL1BridgeRegistry.connect(nonAdmin).revokeRegistrant(registrant)).to.be.revertedWith(
+        'AuthControl: Caller is not an admin'
+      )
+    })
+
+    it('should fail when not granted', async () => {
+      await expect(authControlL1BridgeRegistry.connect(admin).revokeRegistrant(nonRegistrant)).to.be.revertedWith(
+        'already not granted'
+      )
+    })
+
+    it('should revoke registrant', async () => {
+      await authControlL1BridgeRegistry.connect(admin).revokeRegistrant(registrant)
+      expect(await authControlL1BridgeRegistry.isRegistrant(registrant)).to.equal(false)
+    })
+  })
+
+  describe('Test for isOwner', () => {
+    it('should return true for admin', async () => {
+      expect(await authControlL1BridgeRegistry.isOwner()).to.equal(true)
+    })
+
+    it('should return false for non-admin', async () => {
+      expect(await authControlL1BridgeRegistry.connect(nonAdmin).isOwner()).to.equal(false)
+    })
+  })
+
+  describe('Test for supportsInterface', () => {
+    it('should return true for supportsInterface(bytes4)', async () => {
+      expect(await authControlL1BridgeRegistry.supportsInterface('0x01ffc9a7')).to.equal(true)
+    })
+  })
+})
